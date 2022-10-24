df8f6b1bd480da5305cb8cea9e38acd9317554d9
devalamin <alamin.hasibmd2017@gmail.com>
2022-10-25 01:36:13 +0600

    category

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors')
 
+const categories = require('./data/categories.json')
+
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('hello World')
+})
+
+app.get('/courses', (req, res) => {
+    res.send(categories)
 })
 
 app.listen(port, () => {
